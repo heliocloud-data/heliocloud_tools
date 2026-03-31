c13d07aea4ad987dfd27f8fd9121157f566513f2
Sandy Antunes <sandy.antunes@jhuapl.edu>
2026-03-31 14:46:59 -0400

    updated stage3 per Omar

--- src/heliocloud_tools/fetching/cdaweb/stage3_generate_S3_cp.sh
+++ src/heliocloud_tools/fetching/cdaweb/stage3_generate_S3_cp.sh
@@ -4,16 +4,16 @@ echo "Given a list of CDAWeb files to copy (fetch_cdaweb_for_odr.list),"
 echo "  creates script 'fetch_cdaweb_for_odr.sh' that copies via wget into S3,"
 
 source="fetch_cdaweb_for_odr.list"
-dest="fetch_cdaweb_for_odr.curl.sh"
+dest="fetch_cdaweb_to_scratch.sh"
 httpsget="https://spdf.gsfc.nasa.gov"
-s3dest="s3://scratch-data-ops/spdf_$(LC_ALL=C date +%d%b%Y)"
+s3dest="s3://scratch-data-ops/spdf" #_$(LC_ALL=C date +%d%b%Y)"
 
 echo "... setting S3 destination in script to '$s3dest'"
 
 awk '{
   file2 = $0
   sub(/^pub\//, "cdaweb/", file2)
-  print "curl -L " httpsget "/" $0 " | aws s3 cp - " s3dest "/" file2
+  print "curl -sSL " httpsget "/" $0 " | aws s3 cp - " s3dest "/" file2
 }' httpsget="$httpsget" s3dest="$s3dest" "$source" > "$dest"
 
 echo "Done, made script '$dest'."
